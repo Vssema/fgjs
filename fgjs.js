722303a3636ba121f54c5fbe5ae8f7a7c390affa
bringmehome <bringmehome@vip.qq.com>
2015-09-10 16:07:15 +0800

    with v1 and v2

--- fgjs.js
+++ fgjs.js
@@ -3,22 +3,51 @@
 * 2015-09-08
 */
 /**
-遗留问题：
-1、login时候返回userid
-2、解绑某个用户
-3、解绑自己和设备的关系
-4、其他delete的接口
+questions------------------------------------------------------
+1 (ok)login and call backe= userid
+2 remove one user
+3 (ok)unbind myself
+4 delete interfaces
 */
 'use strict';
 var Promise = require("promise");
 var md5 = require("md5");
 var FG = exports = {};
 // const API_ROOT = "http://www.easylink.io/v1/";
-var _userToken, _appId, _appSecret;
+var _appId = "";
+var _appSecret = "";
+var _version = "";
+var _userToken = "";
+var _userID = "";
 
-var end_point = "http://www.easylink.io/v2/";
+var end_point = "http://www.easylink.io/";
+
 // console.log(md5('message'));
-var urls = {
+
+var urls1 = {
+    "user/applyResetpassword" : "post",
+    "user/changepassword" : "post",
+    "user/join" : "post",
+    "user/login" : "post",
+    "user/resetpassword" : "post",
+    "user/revalidate" : "post",
+    "user/validate" : "post",
+
+    "key/authorize" : "post",
+    "key/user/authorize" : "post",
+    
+    "device/find" : "post",
+    "device/devices" : "post",
+    "device/modify" : "post",
+    "device/authorization/devices" : "post",
+    "device/user/delete" : "post",
+    "device/user/query" : "post",
+    "device/delete" : "post",
+
+    "authorization/authorize" : "post",
+};
+
+var urls2 = {
     "users/info" : "get",
     "users/tokens" : "get",
     "users" : "post",
@@ -33,12 +62,11 @@ var urls = {
     "devices/get" : "get",
     "devices/users" : "get",
     "devices/modify" : "put",
-    "devices/users" : "delete",//设备解绑某个用户 有问题
-    "devices/users" : "delete",//设备解绑某个用户 有问题
+    // "devices/users" : "delete",//------------------------------------------------------remove one user
     
     "authorization/devices" : "get",
     "authorization/devices/manage" : "post",
-}
+};
 
 //if the method is get change data to url
 var _eachData = function(data){
@@ -61,6 +89,7 @@ var _ajax = function(method, url, data) {
             url = url+"?"+_eachData(data);
         }else if("delete" == method){
             url = url+"?"+_eachData(data);
+            //------------------------------------------------------check delete type
         }
         console.log(url);
         xhr.open(method, url, true);
@@ -105,21 +134,58 @@ var _changeName = function(name){
 };
 
 //init user information
-FG.init = function(appId, userToken, appSecret) {
-	_userToken = userToken;
+FG.init = function(appId, appSecret, version) {
     _appId = appId;
-	_appSecret = appSecret;
-}
+    _appSecret = appSecret;
+    if(typeof version != "undefined"){
+        _version = version.toLowerCase();
+    }else{
+        _version = "v2";
+    }
 
-//make anonymous functions
-for(var k in urls) {
-    var fk = _changeName(k);
-    // console.log("for = "+fk);
-    FG[fk] = (function (url, method) {
-      return function (param) {
-        return _ajax(method, end_point+url, param);
-        }
-    })(k, urls[k]);
-}
+    //make anonymous functions
+    end_point =  end_point+_version+"/";
+    console.log(end_point);
+
+    var urllist;
+    if("v1" == _version){
+       urllist = urls1;
+    }else if("v2" == _version){
+       urllist = urls2;
+    }
+
+    for(var k in urllist) {
+        var fk = _changeName(k);
+        console.log("for = "+fk);
+        FG[fk] = (function (url, method) {
+          return function (param) {
+            return _ajax(method, end_point+url, param);
+          }
+        })(k, urllist[k]);
+    }
+};
+
+FG.login = function(param){
+    var loginAjax;
+     if("v1" == _version){
+        loginAjax= FG.userLogin(param);
+        loginAjax.then(function(ret){
+    	    _userToken = ret.token;
+            _userID = ret.login_id;
+            // console.log(_userToken);
+            // console.log(_userID);
+        },function(err){});
+     }else if("v2" == _version){
+        loginAjax = FG.usersLogin(param);
+        loginAjax.then(function(ret){
+            _userToken = ret.user_token;
+            _userID = ret.user_id;
+            // console.log(_userToken);
+            // console.log(_userID);
+        },function(err){});    
+     }
+    
+    return loginAjax;
+};
 
 global.FG = FG;
